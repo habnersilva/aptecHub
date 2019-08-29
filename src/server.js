d5c6fe7eabc8086eab5a1a97322df215287f987c
Habner Silva <contato@habnersilva.com.br>
2019-08-29 15:33:02 -0300

    Express rodanddo

---
+++ src/server.js
@@ -0,0 +1,8 @@
+const express = require("express")
+const port = process.env.PORT || 3000
+
+const server = express()
+
+server.listen(port, () => {
+  console.log("Server listening in http://localhost:" + port)
+})
