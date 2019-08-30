779b3819889a6a810ec540c701a0917f118a55b4
Habner Silva <contato@habnersilva.com.br>
2019-08-30 16:44:39 -0300

    Adcionado nodemon

---
+++ src/controllers/dashboard.js
@@ -0,0 +1,7 @@
+const index = (req, res) => {
+  res.render("main")
+}
+
+module.exports = {
+  index
+}
--- src/server.js
+++ src/server.js
@@ -1,7 +1,19 @@
+require("dotenv").config()
+
 const express = require("express")
+const bodyParser = require("body-parser")
+const routers = require("./routers")
+const path = require("path")
 const port = process.env.PORT || 3000
 
-const server = express()
+server = express()
+
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(express.static("./dist/public"))
+server.use(routers)
+
+server.set("view engine", "ejs")
+server.set("views", path.join(__dirname, "views"))
 
 server.listen(port, () => {
   console.log("Server listening in http://localhost:" + port)
