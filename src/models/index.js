f500431aae123dcb3b97f03ab94e22701e1d6f96
Habner Silva <contato@habnersilva.com.br>
2019-09-17 17:48:53 -0300

    Deploy MYSQL_PASSWORD

--- src/models/index.js
+++ src/models/index.js
@@ -5,7 +5,7 @@ const Sequelize = require("sequelize")
 const sequelize = new Sequelize(
   process.env.MYSQL_DBNAME,
   process.env.MYSQL_USER,
-  "",
+  process.env.MYSQL_PASSWORD,
   {
     dialect: "mysql",
     host: process.env.MYSQL_HOST,
