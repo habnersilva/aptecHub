06fe4963f13b11e7826ec3651544a3e8c132d4e4
Habner Silva <contato@habnersilva.com.br>
2019-10-09 13:35:39 -0300

    Adicionado catch em Sequelize

--- src/server.js
+++ src/server.js
@@ -6,11 +6,16 @@ const port = process.env.PORT || 3000
 
 const server = require("./app")(models)
 
-sequelize.sync({ force: false }).then(() => {
-  // Criar o primeiro Usuario
-  initialUser()
+sequelize
+  .sync({ force: false })
+  .then(() => {
+    // Criar o primeiro Usuario
+    initialUser()
 
-  server.listen(port, () => {
-    console.log("Server listening in http://localhost:" + port)
+    server.listen(port, () => {
+      console.log("Server listening in http://localhost:" + port)
+    })
   })
-})
+  .catch(err => {
+    console.log(err)
+  })
