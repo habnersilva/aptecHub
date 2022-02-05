2b0ec32876106615d2442a325e1293b2ef57d85a
Adalberto Taylor <adalbertotaylor84@gmail.com>
2022-02-05 14:47:44 -0300

    Disable tasks temporarily

--- src/tasks/indexTasks.js
+++ src/tasks/indexTasks.js
@@ -1,8 +1,8 @@
 const init = (cron, models) => {
   const tasks = {}
 
-  tasks.autoDownload = require("./autoDownload")(cron, models)
-  tasks.autoSync = require("./autoSync")(cron, models)
+  // tasks.autoDownload = require("./autoDownload")(cron, models)
+  // tasks.autoSync = require("./autoSync")(cron, models)
 
   return tasks
 }
